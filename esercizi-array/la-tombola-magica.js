baa4bfd569a9fc6cce916b33d390a485fa0a6c04
MarcoJavascript <marco.terramagra.mt@gmail.com>
2020-11-17 00:15:08 +0100

    Update la-tombola-magica.js

--- esercizi-array/la-tombola-magica.js
+++ esercizi-array/la-tombola-magica.js
@@ -19,3 +19,49 @@
 
   http://www.imparareaprogrammare.it
 */
+var array = [];
+var numeri = [4, 12, 25, 37, 46, 55, 62, 69, 72, 81];
+var max = 90;
+var min = 1;
+var winning = [];
+var n = Math.round((Math.random()* (max-min)+ min));
+for (var i = 0; i < numeri.length; i++) {
+while (array.includes(n))
+  n = Math.round((Math.random()* (max-min)+ min));
+if (numeri.includes(n)) {
+  winning.push(n);
+  array.push(n);
+}else {
+  array.push(n);
+}
+}
+
+console.log('I tuoi numeri sono:' + ' ' + numeri);
+console.log('I numeri estratti sono:' + ' ' + array);
+var w = winning.length;
+switch (w) {
+case 1:
+console.log(`Peccato! Hai azzeccato solo un numero, cioè ${winning}`);
+break;
+case 2:
+console.log(`Hai fatto ambo! Sono usciti infatti ${winning}`);
+break;
+case 3:
+console.log(`Hai fatto terno! Sono usciti infatti ${winning}`);
+break;
+case 4:
+console.log(`Hai fatto quaterna! Sono usciti infatti ${winning}`);
+break;
+case 5:
+case 6:
+case 7:
+case 8:
+case 9:
+console.log(`Hai fatto cinquina! Sono usciti infatti ${winning}`);
+break;
+case 10:
+console.log('Grande! Hai fatto Tombola!');
+break;
+default:
+console.log('Peccato, non ne hai preso nemmeno uno!');
+}
