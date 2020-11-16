0a178e517a0ecbeeee27b314fee98130adbc667f
MarcoJavascript <marco.terramagra.mt@gmail.com>
2020-11-16 16:34:32 +0100

    Update vinci-tu.js

--- esercizi-cicli/vinci-tu.js
+++ esercizi-cicli/vinci-tu.js
@@ -14,3 +14,27 @@
 
   http://www.imparareaprogrammare.it
 */
+var tiri = 12;
+const player1 = 'Alberto';
+const player2 = 'Lorenzo';
+var punteggio1 = 0;
+var punteggio2 = 0;
+var min = 1;
+var max = 6;
+for (var i = 0; i < tiri; i++) {
+  var m = (Math.random() * (max - min) + min);
+  var result1 = Math.floor(m);
+  punteggio1 = punteggio1 + result1;
+  var v = (Math.random() * (max - min) + min);
+  var result2 = Math.floor(v);
+  punteggio2 = punteggio2 + result2;
+}
+console.log(`${player1}, tirando il dado ${tiri} volte, ha ottenuto ${punteggio1} punti.`);
+console.log(`${player2}, tirando il dado ${tiri} volte, ha ottenuto ${punteggio2} punti`);
+if ( punteggio1 > punteggio2){
+console.log(`Il vincitore è ${player1}`);
+}else if (punteggio1 < punteggio2) {
+  console.log(`Il vincitore è ${player2}`);
+} else if (punteggio1 === punteggio2) {
+  console.log('Pareggio!');
+}
