285aee7f8200fa7a8ee18b5d5126bda71f5991b1
MarcoJavascript <marco.terramagra.mt@gmail.com>
2020-11-19 17:23:47 +0100

    Updates

--- esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
+++ esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
@@ -8,6 +8,20 @@
     2 | 0 | 2 | 4 | 6 | 8 | 10 | 12 | 14 | 16 | 18 | 20
     ...
     10 | 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100
-    
+
   http://www.imparareaprogrammare.it
 */
+var riga = [0,1,2,3,4,5,6,7,8,9,10];
+var colonna = riga.slice();
+var matrix1 = [];
+var s = 'x ';
+for ( var t = 0; t < riga.length; t++){
+  s += `| ${t} `;
+}
+  console.log(s);
+for ( var j = 0; j < riga.length; j++) {
+for ( var i = 0; i < riga.length; i++) {
+matrix1[i] = riga[i] * colonna[j];
+}
+console.log(`${j} | ${matrix1.join(' | ')}`);
+}
--- esercizi-array-multidimensionali/procedi-alla-cieca.js
+++ esercizi-array-multidimensionali/procedi-alla-cieca.js
@@ -7,3 +7,26 @@
 
   http://www.imparareaprogrammare.it
 */
+var matrix = [];
+var array = [];
+var array2 = [];
+var array3 = [];
+var i = 0;
+do {
+  array[i] = Math.round(Math.random() * (100-1) +1);
+  i++;
+}while (array.length < 10);
+var j = 0;
+do {
+  array2[j] = Math.round(Math.random() * (100-1) +1);
+  j++;
+}while (array2.length < 10);
+var t = 0;
+do {
+  array3[t] = Math.round(Math.random() * (100-1) +1);
+  t++;
+}while (array3.length < 10);
+matrix[0] = array;
+matrix[1] = array2;
+matrix[2] = array3;
+console.log(matrix);
