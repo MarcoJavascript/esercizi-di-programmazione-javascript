55f400c2525359db8c63039a8cfdfaf5c1e3db3e
MarcoJavascript <marco.terramagra.mt@gmail.com>
2020-11-20 12:55:39 +0100

    Update il-sottosopra.js

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -3,7 +3,7 @@
   Scrivi un programma che prenda in input un array multidimensionale e stampi in output la sua trasposta.
 
   Esempio:
-    Input: 
+    Input:
       [
         [1, 2],
         [3, 4],
@@ -17,3 +17,17 @@
 
   http://www.imparareaprogrammare.it
 */
+var array = [];
+var array1 = [];
+array[0] = [1,2];
+array[1] = [3,4];
+array[2] = [5,6];
+array[3] = [7,8];
+console.log(array);
+for ( var i = 0; i < 2; i++) {
+  array1[i] = [];
+  for ( var j = 0; j < array.length; j++) {
+    array1[i][j] = array[j][i];
+  }
+}
+console.log(array1);
