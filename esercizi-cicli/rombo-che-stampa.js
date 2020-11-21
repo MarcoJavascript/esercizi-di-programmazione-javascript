af0412e454e3beeb8d36732b8c69eb1033793cac
MarcoJavascript <marco.terramagra.mt@gmail.com>
2020-11-21 12:08:33 +0100

    Update rombo-che-stampa.js

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -13,6 +13,22 @@
 
   Consiglio:
   Per indentare puoi usare il tab con '\t'.
-  
+
   http://www.imparareaprogrammare.it
 */
+var input = 3;
+var tabella = '';
+var spazi;
+for ( var i=1; i <= input*2; i+=2) {
+  if ( input >= i) {
+  spazi = input - i;
+}else spazi = i - input;
+for ( var j = 1; j <= spazi/2; j++) {
+  tabella += ' ';
+}
+for (var j = 1; j <= input - spazi; j++) {
+  tabella += 'x';
+}
+  tabella += '\n';
+}
+console.log(tabella);
